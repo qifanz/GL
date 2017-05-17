e0e35027c31df402ca48b57d55d9830476c23430
pchauvet <pchauvet@IF501-210-12>
2017-05-17 16:33:28 +0200

    Correction de ServiceAnalyse

--- AnalyseGenome/AnalyseGenome/ServiceAnalyse.cpp
+++ AnalyseGenome/AnalyseGenome/ServiceAnalyse.cpp
@@ -6,8 +6,8 @@
 void ServiceAnalyse::AnalyseCiblee(Analyse& a, string maladie)
 {
 	//Utiliser find pour trouver le pair<maladie,mots> pour la maladie donnée dans la map.
-	genStart = a.genome.mots.begin;
-	genStop = a.genome.mots.end;
+	genStart = a.genome.mots.begin();
+	genStop = a.genome.mots.end();
 	DICO_IT tuple = dictionnaire.find(maladie);
 	ParcoursGenome(tuple, a);
 }
@@ -15,16 +15,16 @@ void ServiceAnalyse::AnalyseCiblee(Analyse& a, string maladie)
 void ServiceAnalyse::AnalyseGenerale(Analyse& a)
 {
 	//Utiliser un iterator pour parcourir toute la map. Pour chaque pair, lancer une analyse
-	genStart = a.genome.mots.begin;
-	genStop = a.genome.mots.end;
-	for (DICO_IT tuple = dictionnaire.begin; tuple != dictionnaire.end; tuple++) 
+	genStart = a.genome.mots.begin();
+	genStop = a.genome.mots.end();
+	for (DICO_IT tuple = dictionnaire.begin(); tuple != dictionnaire.end(); tuple++) 
 	{
 		ParcoursGenome(tuple, a);
 	}
 }
 
 void ServiceAnalyse::ParcoursGenome(DICO_IT tuple, Analyse& a) {
-	for (MOTS_IT i = (*tuple).second.begin; i != (*tuple).second.end; i++) 
+	for (MOTS_IT i = (*tuple).second.begin(); i != (*tuple).second.end(); i++) 
 	{
 		for (GENO_IT gen = genStart; gen != genStop; gen++) 
 		{
@@ -40,7 +40,7 @@ set<string> ServiceAnalyse::getListeMaladies()
 {
 	//Utiliser un iterator pour parcourir toute la map. Renvoie un set correspondant a la liste des maladies.
 	set<string> listeMaladies;
-	for(DICO_IT i = dictionnaire.begin; i!=dictionnaire.end; i++)
+	for(DICO_IT i = dictionnaire.begin(); i!=dictionnaire.end(); i++)
 	{
 		listeMaladies.insert((*i).first);
 	}
