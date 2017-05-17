39edaa548deea0b447b000109d057428c8ffc7e9
pchauvet <pchauvet@IF501-210-12>
2017-05-17 15:47:47 +0200

    ServiceAnalyse : AnalyseCiblee et AnalyseGenerale implémentées.

--- AnalyseGenome/AnalyseGenome/ServiceAnalyse.cpp
+++ AnalyseGenome/AnalyseGenome/ServiceAnalyse.cpp
@@ -1,19 +1,38 @@
 #include "stdafx.h"
 #include "ServiceAnalyse.h"
 
-// POUR LES ANALYSES :  Pour chaque pair parcouru, comparer la list de mots correspondante avec find() dans le multiset du génome. Rajouter un resultat dans l'analyse avec addResult(String maladie) pour chaque mot trouv?dans le génome. Seul le parcours change selon le type d'analyse.
+// POUR LES ANALYSES :  Pour chaque pair parcouru, comparer la list de mots correspondante avec find() dans le multiset du génome. Rajouter un resultat dans l'analyse avec addResult(String maladie) pour chaque mot trouvé dans le génome. Seul le parcours change selon le type d'analyse.
 
-void AnalyseCiblee(Analyse& a, string maladie)
+void ServiceAnalyse::AnalyseCiblee(Analyse& a, string maladie)
 {
 	//Utiliser find pour trouver le pair<maladie,mots> pour la maladie donnée dans la map.
+	genStart = a.genome.mots.begin;
+	genStop = a.genome.mots.end;
+	DICO_IT tuple = dictionnaire.find(maladie);
+	ParcoursGenome(tuple, a);
 }
 
-void AnalyseGenerale(Analyse& a)
+void ServiceAnalyse::AnalyseGenerale(Analyse& a)
 {
-	//Utiliser un iterator pour parcourir toute la map. Pour chaque pair, lancer une analyse ciblée
+	//Utiliser un iterator pour parcourir toute la map. Pour chaque pair, lancer une analyse
+	genStart = a.genome.mots.begin;
+	genStop = a.genome.mots.end;
+	for (DICO_IT tuple = dictionnaire.begin; tuple != dictionnaire.end; tuple++) {
+		ParcoursGenome(tuple, a);
+	}
 }
 
-set<String> getListeMaladies()
+void ServiceAnalyse::ParcoursGenome(DICO_IT tuple, Analyse& a) {
+	for (MOTS_IT i = (*tuple).second.begin; i != (*tuple).second.end; i++) {
+		for (GENO_IT gen = genStart; gen != genStop; gen++) {
+			if ((*i) == (*gen)) {
+				a.addResult(*i);
+			}
+		}
+	}
+}
+
+set<string> ServiceAnalyse::getListeMaladies()
 {
 	//Utiliser un iterator pour parcourir toute la map. Renvoie un set correspondant a la liste des maladies.
 }
--- AnalyseGenome/AnalyseGenome/ServiceAnalyse.h
+++ AnalyseGenome/AnalyseGenome/ServiceAnalyse.h
@@ -1,23 +1,39 @@
 #pragma once
-#include <map>
 #include <list>
-#include <string>
+#include <set>
+#include <iterator>
 #include"Analyse.h"
 
 using namespace std;
 
+typedef string MALADIE;
+
+typedef list<string> MOTS;
+typedef MOTS::iterator MOTS_IT;
+
+typedef map<MALADIE, MOTS> DICO;
+typedef DICO::iterator DICO_IT;
+
+typedef multiset<string>::iterator GENO_IT;
+
+
 class ServiceAnalyse
 {
 public:
-
+	
 	void AnalyseCiblee(Analyse& a, string maladie);
 	void AnalyseGenerale(Analyse& a);
+	set<string> getListeMaladies();
 	ServiceAnalyse();
 	~ServiceAnalyse();
 
 private :
-	map< string maladie, list<string> mot >;
-	Genome genomeTemp;
+	void ParcoursGenome(DICO_IT tuple, Analyse& a);
+
+	DICO dictionnaire;
+	//genStart et genStop définissent les bornes du génome. Evite de les calculer à chaque appel de ParcoursGenome.
+	GENO_IT genStart;	
+	GENO_IT genStop;
 
 };
 
